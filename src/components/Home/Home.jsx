0571ecbbe6a3cdcaae36601174d28db7de5cd3ba
istiakantu <istakahmed636@gmail.com>
2023-04-07 01:10:19 +0600

    route added and data loaded

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const tShirts = useLoaderData()
     return (
         <div>
-            <h2>This is HOMe</h2>
+            <h2>This is Home: {tShirts.length}</h2>
         </div>
     );
 };
