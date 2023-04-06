aa4c6a7be4cf03cb420dbc7c8d42053bec664e66
istiakantu <istakahmed636@gmail.com>
2023-04-07 01:51:32 +0600

    diplay t-shirt

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css'
 
 const Home = () => {
     const tShirts = useLoaderData()
+
+    const [cart, setCart]= useState([]);
+
+    const handelAddToCart = tShirt => {
+        console.log(tShirt)
+    }
+    
     return (
-        <div>
-            <h2>This is Home: {tShirts.length}</h2>
+        <div className='home-container'>
+            <div className="t-shirts-container">
+                {
+                    tShirts.map(tShirt=><Tshirt
+                    key={tShirt._id}
+                    tShirt={tShirt}
+                    handelAddToCart={handelAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
+        
     );
 };
 
---
+++ src/components/Tshirt/Tshirt.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Tshirt.css'
+
+const Tshirt = ({tShirt,handelAddToCart}) => {
+    const {_id, picture, name, price, gender}= tShirt;
+    return (
+        <div className='t-shirt-container'>
+            <img src={picture} alt="" />
+            <p>{name}</p>
+            <p>Price: ${price}</p>
+            <button onClick={handelAddToCart}>Buy Now</button>
+        </div>
+    );
+};
+
+export default Tshirt;
