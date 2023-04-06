9fb8d05402884908a06f2538901dbcc5242b9dcc
istiakantu <istakahmed636@gmail.com>
2023-04-07 01:06:00 +0600

    started

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Root from './components/Layout/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/order',
+        element: <h2>Hiiiii</h2>,
+      },
+      {
+        path: '/about',
+        element: <h2>About Us</h2> ,
+      },
+      {
+        path: '/contact',
+        element: <h2>Contact Us</h2>,
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
